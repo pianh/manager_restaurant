d632f767e53bc3ab6b9066407a0cf2ff30793df2
AnhThu_Sherry <79581404+ThuNguyen3108@users.noreply.github.com>
2023-03-03 23:14:37 +0700

    update
    admin

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ app.get('/register', (req, res) => {
 app.get('/cart', (req, res) => {
   res.render('cart');
 })
-
 app.get('/checkout', (req, res) => {
   res.render('checkout');
 })
@@ -70,40 +69,104 @@ app.get('/admin_order', (req, res) => {
   res.render('admin/order');
 })
 
+// Thêm món ăn
+
 app.get('/admin_addFood', (req, res) => {
   res.render('admin/addFood');
 })
+
+// Chỉnh sửa món ăn
+app.get('/admin_editFood', (req, res) => {
+  res.render('admin/edit_Food');
+})
+
+//Thêm thành phần
+app.get('/admin_addingredientList', (req, res) => {
+  res.render('admin/addingredientList');
+})
+
+//Sửa thành phần
+app.get('/admin_editingredientList', (req, res) => {
+  res.render('admin/edit_IngredientList');
+})
+
+// khách hàng
 
 app.get('/admin_customer', (req, res) => {
   res.render('admin/customerList');
 })
 
+// thêm manager
+app.get('/admin_addManager', (req, res) => {
+  res.render('admin/addManager');
+})
+
+//chỉnh sửa manager
+app.get('/admin_editManager', (req, res) => {
+  res.render('admin/edit_Manager');
+})
+
+//thêm tiệc
+app.get('/admin_addParty', (req, res) => {
+  res.render('admin/addParty');
+})
+
+//chỉnh sửa tiệc
+app.get('/admin_editParty', (req, res) => {
+  res.render('admin/edit_Party');
+})
+
+//thêm nhà hàng
+app.get('/admin_addRestaurant', (req, res) => {
+  res.render('admin/addRestaurant');
+})
+
+//chỉnh sửa nhà hàng
+app.get('/admin_editRestaurant', (req, res) => {
+  res.render('admin/edit_Restaurant');
+})
+
+//thêm bàn
+app.get('/admin_addTable', (req, res) => {
+  res.render('admin/addTable');
+})
+
+//chỉnh sửa bàn
+app.get('/admin_editTable', (req, res) => {
+  res.render('admin/edit_Table');
+})
+
+//thành phần
 app.get('/admin_ingredientList', (req, res) => {
   res.render('admin/ingredientList');
 })
 
+//bàn
 app.get('/admin_table', (req, res) => {
   res.render('admin/tableList');
 })
 
+//loại tiệc
 app.get('/admin_typeOfpartyList', (req, res) => {
   res.render('admin/typeOfpartyList');
 })
 
+//món ăn
 app.get('/admin_food', (req, res) => {
   res.render('admin/food');
 })
 
+//đặt bàn
 app.get('/admin_reserveTable', (req, res) => {
   res.render('admin/reserveTable');
 })
 
-
+//nhà hàng
 app.get('/admin_restaurant', (req, res) => {
   res.render('admin/restaurants');
 })
 
-
+//quản lý
 app.get('/admin_manager', (req, res) => {
   res.render('admin/manager');
 })
