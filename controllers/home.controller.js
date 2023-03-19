a03e826d16c8f741f344d9d239e05dbf98dd1b80
AnhThu_Sherry <79581404+ThuNguyen3108@users.noreply.github.com>
2023-03-19 16:13:47 +0700

    update
    update

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -34,6 +34,7 @@ class HomeController{
     }
    
     async cart(req, res){
+        const cookies = new CookieProvider();
         cookies.setParamater(req, res);
         let bString = cookies.getCookie("carts");
         let b = [];
@@ -46,14 +47,15 @@ class HomeController{
             let itemCart = {
                 food: await foodsService.selectById(item.food),
                 quantity: item.quantity
-        };
-        if(itemCart.food != null){
-            carts.push(itemCart);
-
+            }
+            if(itemCart.food != null){
+                carts.push(itemCart);
+    
+            }
         }
         res.render('cart', {carts:carts, formatPrice:formatPrice});
+    
     }
-}
 
     async addCart(req, res){
         cookies.setParamater(req, res);
@@ -98,44 +100,44 @@ class HomeController{
       
         res.redirect('/cart');
     }
+    async clearAllCart(req, res){
+        cookies.setParamater(req, res); 
+        cookies.clearCookie("carts");
+        res.redirect('/cart');
+    }
 
     async updateCart(req, res){
         cookies.setParamater(req, res); 
-        if(cookies.getCookie("carts")){
             let bString = cookies.getCookie("carts");
-            let b = JSON.parse(bString);
-            let food = req.body.foodID;
-            let bFood = b.map(x=>x.food);
-            let indexOfFoodInFoodArray = bFood.indexOf(food);
-            if(indexOfFoodInFoodArray != -1){
-                b.splice(indexOfFoodInFoodArray, 1);
-                bString = JSON.stringify(b);
-                cookies.setCookie("carts", bString, 24);
+            if(bString != undefined){
+                let b = JSON.parse(bString);
+                let food = req.body.foodID;
+                let bFood = b.map(x=>x.food);
+                let indexOfFoodInFoodArray = bFood.indexOf(food);
+                if(indexOfFoodInFoodArray != -1){
+                    b.splice(indexOfFoodInFoodArray, 1);
+                    let food = req.body.foodID;
+                    let quantity = req.body.quantity;
+                    let itemCart = {
+                            food: food,
+                            quantity: quantity
+                    };
+                    b.push(itemCart);
+                    bString = JSON.stringify(b);
+                    cookies.setCookie("carts", bString, 24);
+                }
             }
             
-        }
-        let food = req.body.foodID;
-        let quantity = req.body.quantity;
-        let itemCart = {
-                food: food,
-                quantity: quantity
-        };
-        if(cookies.getCookie("carts")){
-            let bString = cookies.getCookie("carts");
-            let b = JSON.parse(bString);
-            b.push(itemCart);
-            bString = JSON.stringify(b);
-            cookies.setCookie("carts", bString, 24);
-        }
-        else{
-            let a = [];
-            a.push(itemCart);
-            let aString = JSON.stringify(a);
-            cookies.setCookie("carts", aString, 24);
-        }
+            
+        
+        
         res.redirect('/cart');
 
 
+    }
+
+    async checkOut(req, res){
+       res.render('checkout')
     }
 
     //Ham xu ly
--- routes/home.route.js
+++ routes/home.route.js
@@ -11,9 +11,13 @@ router.route("/register").get(home.register).post(home.registerHandle);
 router.route("/food/:id").get(home.detailFood);
 
 router.route("/cart").get(home.cart);
-router.route("/addToCart").post(home.addCart);
+router.route("/addToCart").post(home.addCart);  
 
 router.route("/updateToCart").post(home.updateCart); 
+router.route("/removeFromCart").post(home.removeCart); 
+router.route("/clearAllProductInCart").get(home.clearAllCart);
+
+router.route("/checkOut").get(home.checkOut);
 
 router.route("/").get(home.index);
 
